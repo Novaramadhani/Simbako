519610498e3ae3c134b47d37e512dacf9bbc5704
Nova <novaaa218@gmail.com>
2026-06-03 11:56:08 +0700

    tes

---
+++ Simbako/DatabaseHellper/database.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using Npgsql;
+
+namespace Simbako.DatabaseHellper
+{
+   public static class database
+    {
+        public static NpgsqlConnection GetConnection()
+        {
+            string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=novaria;Database=simbako";
+            return new NpgsqlConnection(connectionString);
+        }
+    }
+}
