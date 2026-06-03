750a58cbeddd3af2e84ba71c17ae406dc0df0925
jaidanhatemel <mzgcoc@gmail.com>
2026-06-03 12:37:11 +0700

    Buat1

---
+++ Simbako/DBConnection.cs
@@ -0,0 +1,16 @@
+﻿using Npgsql;
+
+namespace SIMBAKO
+{
+    public class DBConnection
+    {
+        // Ganti sesuai pengaturan PostgreSQL kamu
+        private static string connString =
+            "Host=localhost;Port=5432;Database=simbako;Username=postgres;Password=12345";
+
+        public static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(connString);
+        }
+    }
+}
